8cb902d206e76ce4c32dd59d6dc4e1ea89e804f4
Benjamin Milde <benni@kobrakai.de>
2020-09-02 18:19:45 +0200

    Update notification.ex

--- lib/notti/notification.ex
+++ lib/notti/notification.ex
@@ -6,6 +6,6 @@ defprotocol Notti.Notification do
   @spec receiving_parties(t, Keyword.t() | map) :: list(ReceivingParty.t())
   def receiving_parties(notificiation, opts)
 
-  @spec build_for_channel(t, ReceivingParty.t(), Channel.t(), map, Keyword.t() | map) :: list()
+  @spec build_for_channel(t, ReceivingParty.t(), Channel.t(), map, Keyword.t() | map) :: term
   def build_for_channel(notification, channel, party, details, opts)
 end
